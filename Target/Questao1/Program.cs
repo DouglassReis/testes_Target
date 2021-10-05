707dd19ac6e668e3175b1ad5a53a242b7fc29198
DouglassReis <douglas.reiscrq@gmail.com>
2021-10-04 23:01:35 -0300

    Commit dos testes

---
+++ Target/Questao1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Target
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int indice = 12;
+            int k = 0;
+            int soma = 0;
+
+            while (k < indice)
+            {
+                k += 1;
+                soma += k;
+            }
+
+            Console.WriteLine(soma);
+        }
+    }
+}
---
+++ Target/Questao2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Questao2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long number = long.Parse(Console.ReadLine());
+
+            if (Fibonnaci(number))
+            {
+                Console.WriteLine("O número pertence a sequência de Fibonacci");
+            }
+            else
+            {
+                Console.WriteLine("O número NÃO pertence a sequência de Fibonacci");
+            }
+         }
+
+        static bool Fibonnaci(long number)
+        {
+            long n1 = 0;
+            long n2 = 1;
+            long aux;
+            bool validador = (number == 0 || number == 1) ? true : false;
+
+            while (n2 < number)
+            {
+                aux = n2;
+                n2 += n1;
+                n1 = aux;
+                if (n2 == number)
+                {
+                    validador = true;
+                }
+                
+            }
+            return validador;
+        }
+    }
+}
